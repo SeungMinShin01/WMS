e2303314d35649fdbb62037d362595a9b83f342f
SeungMinShin01 <harry9096@gmail.com>
2026-09-25 18:27:38 +0900

    feat(outbound): 출고 문서 목록 조회 API (ED - 12)

---
+++ wms-core/src/main/java/com/wms/controller/outbound/OutboundController.java
@@ -0,0 +1,23 @@
+package com.wms.controller.outbound;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.wms.model.dto.OutboundListDto;
+import com.wms.service.OutboundService;
+
+@RestController
+@RequestMapping("/wms/outbounds")
+public class OutboundController {
+    @Autowired
+    private OutboundService outboundService;
+
+    // 출고 문서 목록
+    @GetMapping("")
+    public List<OutboundListDto> findAll() {
+        return outboundService.findAll();
+    }
+}
