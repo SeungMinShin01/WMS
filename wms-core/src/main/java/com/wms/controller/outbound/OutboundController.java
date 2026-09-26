9fd7fc1d1c2a94005f006bf564d87496f9aee58f
jihwangim09-design <jihwangim09@gmail.com>
2026-09-27 02:24:47 +0900

    feat(outbound): 출고 문서 상세 조회 API (ED-17)

--- wms-core/src/main/java/com/wms/controller/outbound/OutboundController.java
+++ wms-core/src/main/java/com/wms/controller/outbound/OutboundController.java
@@ -4,29 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wms.model.dto.outbound.OutboundDetailDto;
 import com.wms.model.dto.outbound.OutboundListDto;
 import com.wms.service.OutboundService;
 
-import lombok.RequiredArgsConstructor;
 
-@RestController 
-@RequiredArgsConstructor
+@RestController
+@RequestMapping("/wms/outbounds")
 public class OutboundController {
-
-    private final OutboundService outboundService;
+    @Autowired private OutboundService outboundService;
 
     // ED-12 출고 문서 목록 조회
-    @GetMapping ("/wms/outbounds")
+    @GetMapping
     public List<OutboundListDto> getOutboundList() {
         return outboundService.getOutboundList();
     }
 
     // ED-17 출고 문서 상세 조회
-    @GetMapping ("/wms/outbounds/{documentId}")
-    public List<OutboundDetailDto> getOutboundDetailList(){
-        return outboundService.getOutboundDetailList();
+    @GetMapping ("/{documentId}")
+    public OutboundDetailDto getOutboundDetail(@PathVariable (name = "documentId") Integer documentId){
+        return outboundService.getOutboundDetail(documentId);
     }
 }
--- wms-core/src/main/java/com/wms/service/OutboundService.java
+++ wms-core/src/main/java/com/wms/service/OutboundService.java
@@ -2,28 +2,46 @@ package com.wms.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.wms.model.dto.outbound.OutboundDetailDto;
+import com.wms.model.dto.outbound.OutboundItemDto;
 import com.wms.model.dto.outbound.OutboundListDto;
 import com.wms.model.entity.DocumentEntity;
+import com.wms.model.entity.DocumentItemEntity;
+import com.wms.model.repository.DocumentItemRepository;
 import com.wms.model.repository.DocumentRepository;
 
-import lombok.RequiredArgsConstructor;
-
-@Service 
-@RequiredArgsConstructor 
+@Service
+@Transactional 
 public class OutboundService {
-    private DocumentRepository documentRepository;
+    @Autowired private DocumentRepository documentRepository;
+    @Autowired private DocumentItemRepository documentItemRepository;
     
     // ED-12 출고 문서 목록 조회
     public List<OutboundListDto> getOutboundList() {
+        // 문서타입이 OUTBOUND만 가져오기
         List<DocumentEntity> documentEntities = documentRepository.findByTypeOrderByExpectedAtAsc("OUTBOUND");
         List<OutboundListDto> documentDtos = documentEntities.stream().map((entity) -> {return OutboundListDto.from(entity);}).toList();
         return documentDtos;
     }
 
     // ED-17 출고 문서 상세 조회
-    public List<OutboundDetailDto> getOutboundDetailList() {
+    public OutboundDetailDto getOutboundDetail(Integer documentId) {
+        DocumentEntity documentEntity = documentRepository.findById(documentId).orElse(null);
+        OutboundDetailDto outboundDetailDto = OutboundDetailDto.from(documentEntity);
+
         
+        List<DocumentItemEntity> documentItemEntities = documentItemRepository.findAll();
+        documentItemEntities.forEach((documentItemEntity) -> {
+        
+        if (documentItemEntity.getDocumentEntity().getDocumentId().equals(documentId)) {
+            OutboundItemDto outboundItemDto = OutboundItemDto.from(documentItemEntity);
+            outboundDetailDto.getItems().add(outboundItemDto);
+        }
+    });
+    return outboundDetailDto;
     }
 }
