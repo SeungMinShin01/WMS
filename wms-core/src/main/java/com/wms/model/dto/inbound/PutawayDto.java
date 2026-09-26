171dfe90df6b73fc6e96e10be1e0ce0e309dac62
hanwooya361 <johunwoo123456@gmail.com>
2026-09-26 18:49:27 +0900

    fix(inbound): PutawayDto lombok import 누락 수정 (ED-16)

--- wms-core/src/main/java/com/wms/model/dto/inbound/PutawayDto.java
+++ wms-core/src/main/java/com/wms/model/dto/inbound/PutawayDto.java
@@ -1,10 +1,15 @@
 package com.wms.model.dto.inbound;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@Builder
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Data 
+@Builder 
 public class PutawayDto {
     private Integer detailId;
     private Integer locationId;
-}
+} 
